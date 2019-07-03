9e0a9aa3a606a93819e423b43afa5121b463961a
srujanm13 <srujanm@MTLP-528.in.mouritech.net>
2019-07-03 22:18:53 +0530

    adding TestnG Classes

--- src/test/java/sampletests/FirstTest.java
+++ src/test/java/sampletests/FirstTest.java
@@ -1,16 +1,18 @@
 package sampletests;
 
-import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 
-public class Tc1 {
+public class FirstTest {
 @Test
 	public void open() {
 	System.setProperty("webdriver.chrome.driver", "D:\\Learn\\WorkSpace\\GitUpload\\stanleytools.com\\Drivers\\chromedriver.exe");
 		WebDriver d=new ChromeDriver();
 		d.get("http://www.google.com");
+		d.close();
 		
 	}
 	
