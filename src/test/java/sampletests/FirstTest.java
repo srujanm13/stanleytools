fba0df690cef0780f0067e0034951fdd1cd24902
srujanm13 <srujanm@MTLP-528.in.mouritech.net>
2019-07-04 11:47:42 +0530

    changes to code

--- src/test/java/sampletests/FirstTest.java
+++ src/test/java/sampletests/FirstTest.java
@@ -2,6 +2,7 @@ package sampletests;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -21,7 +22,7 @@ public void open2() {
 		WebDriver d=new ChromeDriver();
 		d.get("http://www.google.com");
 		d.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[2]/div[1]/div[1]/div[1]/div[1]/input[1]")).sendKeys("FaceBook");
-		d.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[2]/div[1]/div[3]/center[1]/input[1]")).click();
+		d.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[2]/div[1]/div[3]/center[1]/input[1]")).sendKeys(Keys.ENTER);
 		d.close();
 	}
 }
